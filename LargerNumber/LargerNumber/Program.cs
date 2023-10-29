0eb4cc5561f43cf34019191b18d3b81ebc39e8f5
SimeonPetr0v <simeonp925@gmail.com>
2023-10-29 14:43:03 +0200

    Exercises

---
+++ LargerNumber/LargerNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace LargerNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+       
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            GetMax(firstNum, secondNum);
+        }
+        static void GetMax(int firstNum, int secondNum)
+        {
+            if(firstNum > secondNum)
+            {
+                Console.WriteLine(firstNum);
+            }
+            else
+            {
+                Console.WriteLine(secondNum);
+            }
+        }
+    }
+}
